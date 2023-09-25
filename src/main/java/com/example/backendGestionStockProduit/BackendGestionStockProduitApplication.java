fb626565dc88ef37a0564d84f792dd9566f981d8
meryem <laasirimeryem@gmail.com>
2023-09-25 22:46:11 +0200

    second commit

---
+++ src/main/java/com/example/backendGestionStockProduit/BackendGestionStockProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendGestionStockProduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionStockProduitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendGestionStockProduit/BackendGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendGestionStockProduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionStockProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
